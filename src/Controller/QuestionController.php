a87f6fbdbc6bf8b1974d89b5185f2bb94b83ee8e
JesuRed <jesusalfredorv@ufps.edu.co>
2022-03-24 12:05:42 -0500

    cache with acpu and parser light

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -2,9 +2,11 @@
 
 namespace App\Controller;
 
+use Knp\Bundle\MarkdownBundle\MarkdownParserInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Contracts\Cache\CacheInterface;
 use Twig\Environment;
 
 class QuestionController extends AbstractController
@@ -22,18 +24,26 @@ class QuestionController extends AbstractController
     /**
      * @Route("/questions/{anywordhere}", name="app_question_show")
      */
-    public function show($anywordhere)
+    public function show($anywordhere, MarkdownParserInterface $markdownParser, CacheInterface $cache)
     {
         $answers = [
-            'Fear is a tool. When that light hits the sky, it’s not just a call. It’s a warning. For them. 🦇',
+            'Fear is a tool. When that light hits the `sky`, it’s not just a call. It’s a warning. For them. 🦇',
             'I can take care of myself. 🐱‍👤',
             'Get out of here or that suit’s gonna be full of blood. 🤣',
         ];
 
-        dump($this);
+        $questionText = 'I\'ve been turned into a cat, any thoughts on how to turn back? While I\'m **adorable**, I don\'t really care for cat food.';
+        $parsedQuestionText = $cache->get('markdown_' . md5($questionText), function () use ($questionText, $markdownParser) {
+            return $markdownParser->transformMarkdown($questionText);
+        });
+
+        dump($cache);
+        //dd($markdownParser);
+        //dump($this);
 
         return $this->render('question/show.html.twig', [
             'question' => ucwords(str_replace('-', ' ', $anywordhere)),
+            'questionText' => $parsedQuestionText,
             'answers' => $answers,
         ]);
 
