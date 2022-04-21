1bf3fddae300f72417b2529fcaf76b2372e8b129
JesuRed <jesusalfredorv@ufps.edu.co>
2022-04-21 12:00:53 -0500

    set votes

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -3,8 +3,10 @@
 namespace App\Controller;
 
 use App\Entity\Question;
+use App\Repository\QuestionRepository;
 use App\Service\MarkdownHelper;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -25,11 +27,8 @@ class QuestionController extends AbstractController
     /**
      * @Route("/", name="app_homepage")
      */
-    public function homepage(EntityManagerInterface $entityManager)
+    public function homepage(QuestionRepository $repository)
     {
-        $repository = $entityManager->getRepository(Question::class);
-        //dd($repository);
-        //$questions = $repository->findBy([], ['askedAt' => 'DESC']);
         $questions = $repository->findAllAskedOrderedByNewest();
 
         return $this->render('question/homepage.html.twig', [
@@ -44,7 +43,7 @@ class QuestionController extends AbstractController
     {
         $question = new Question();
         $question->setName('Missing pants')
-            ->setanywordhere('missing-pants-'.rand(0, 1000))
+            ->setanywordhere('missing-pants-' . rand(0, 1000))
             ->setQuestion(<<<EOF
 Hi! So... I'm having a *weird* day. Yesterday, I cast a spell
 to make my dishes wash themselves. But while I was casting it,
@@ -61,6 +60,7 @@ EOF
         if (rand(1, 10) > 2) {
             $question->setAskedAt(new \DateTimeImmutable(sprintf('-%d days', rand(1, 100))));
         }
+        $question->setVotes(rand(-20, 50));
 
         $entityManager->persist($question);
         $entityManager->flush();
@@ -75,28 +75,36 @@ EOF
     /**
      * @Route("/questions/{anywordhere}", name="app_question_show")
      */
-    public function show($anywordhere, EntityManagerInterface $entityManager)
+    public function show(Question $question)
     {
         if ($this->isDebug) {
             $this->logger->info('We are in debug mode!');
         }
-
-        $repository = $entityManager->getRepository(Question::class);
-        /** @var Question|null $question */
-        $question = $repository->findOneBy(['anywordhere' => $anywordhere]);
-        if (!$question) {
-            throw $this->createNotFoundException(sprintf('no question found for anywordhere "%s"', $anywordhere));
-        }
-
         $answers = [
             'Make sure your cat is sitting `purrrfectly` still ?',
             'Honestly, I like furry shoes better than MY cat',
             'Maybe... try saying the spell backwards?',
         ];
-
         return $this->render('question/show.html.twig', [
             'question' => $question,
             'answers' => $answers,
+        ]);
+    }
+
+    /**
+     * @Route("/questions/{anywordhere}/vote", name="app_question_vote", methods="POST")
+     */
+    public function questionVote(Question $question, Request $request, EntityManagerInterface $entityManager)
+    {
+        $direction = $request->request->get('direction');
+        if ($direction === 'up') {
+            $question->upVote();
+        } elseif ($direction === 'down') {
+            $question->downVote();
+        }
+        $entityManager->flush();
+        return $this->redirectToRoute('app_question_show', [
+            'anywordhere' => $question->getAnywordhere()
         ]);
     }
 }
